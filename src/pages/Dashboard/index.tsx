a2441bd5f3eba2d5fd45239709617f33fb22c1ec
Igor Dias <igordiasth@gmail.com>
2020-05-06 18:33:25 +1200

    formatValue, list total balance inside the cards

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -36,8 +36,24 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
       const response = await api.get('/transactions');
-      setTransactions(response.data.transactions);
+
+      const transactionsFormatted = response.data.transactions.map(
+        (transaction: Transaction) => ({
+          ...transaction,
+          formattedValue: formatValue(transaction.value),
+        }),
+      );
+
+      const balanceFormatted = {
+        income: formatValue(response.data.balance.income),
+        outcome: formatValue(response.data.balance.outcome),
+        total: formatValue(response.data.balance.total),
+      };
+
+      setTransactions(transactionsFormatted);
+      setBalance(balanceFormatted);
     }
+
     loadTransactions();
   }, []);
 
@@ -51,21 +67,21 @@ const Dashboard: React.FC = () => {
               <p>Income</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Expenses</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Amount</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
@@ -81,14 +97,18 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              {transactions.map(transaction => (
-                <tr>
-                  <td className="title">{transaction.title}</td>
-                  <td className="income">{transaction.value}</td>
-                  {/* <td>{transaction.category}</td> */}
-                  <td>{transaction.created_at}</td>
-                </tr>
-              ))}
+              <tr>
+                <td className="title">Computer</td>
+                <td className="income">R$ 5.000,00</td>
+                <td>Sell</td>
+                <td>20/04/2020</td>
+              </tr>
+              <tr>
+                <td className="title">Website Hosting</td>
+                <td className="outcome">- R$ 1.000,00</td>
+                <td>Hosting</td>
+                <td>19/04/2020</td>
+              </tr>
             </tbody>
           </table>
         </TableContainer>
